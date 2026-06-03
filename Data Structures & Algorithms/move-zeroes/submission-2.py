aaa30e1e51cd50219683e4361366d597d65d62a2
Paarul Ingale <ingalepaarul@gmail.com>
2026-06-03 09:54:07 +0530

    Add: move-zeroes - submission-2

---
+++ Data Structures & Algorithms/move-zeroes/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n =len(nums)
+        j = -1
+        for ind in range (0 , n):
+            if nums[ind]== 0:
+                j = ind
+                break
+
+        for i in range (j+1 , n):
+            if nums[i]!=0 and nums[j]==0:
+                nums[i] , nums[j] = nums[j] ,nums[i]
+                j+=1
+            else:
+                pass
